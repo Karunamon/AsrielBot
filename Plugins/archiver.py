a848c6b0dd81e967da780c1251712d6bb9cf733e
Michael Parks <mparks@tkware.info>
2017-01-22 21:57:02 -0700

    Add archiving plugin

---
+++ Plugins/archiver.py
@@ -0,0 +1,38 @@
+import irc3
+import archiveis
+from irc3.plugins.command import Commands, command
+from Plugins import PluginConfig, MessageRetargeter
+
+
+@irc3.plugin
+class Archiver(object):
+    def __init__(self, bot):
+        self.cfg = PluginConfig(self)
+        self.domains = self.cfg.get('domains').split(',')
+        self.bot = bot
+        irc3.base.logging.log(irc3.base.logging.WARN,
+                              "Archiver ready. Auto trigger on: %s" % self.domains
+                              )
+        mtt = MessageRetargeter(bot)
+        self.msg = mtt.msg
+
+    @command
+    def archive(self, mask, target, args):
+        """
+        Generate an archive.is link for a website.
+
+        Usage:
+            %%archive <URL>
+        """
+        self.msg(mask, target, "Saving that...")
+        url = archiveis.capture(args['<URL>'])
+        self.msg(mask, target, args['<URL>'] + " ==Archived==> " + url)
+
+    @irc3.event(irc3.rfc.PRIVMSG)  # Triggered on every message anywhere.
+    def auto_archive(self, target, mask, data, event):
+        del event
+        for domain in self.domains:
+            if domain.lower() in data:
+                self.bot.get_plugin(Commands).on_command(
+                    cmd='archive', mask=mask, target=target, data=data
+                )
